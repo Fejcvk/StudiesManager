4d85bd10c869a24e803eba5189ef37131480bc8c
Fejcvk <tomaszfejcak@icloud.com>
2017-05-26 16:09:00 +0200

    first commit

---
+++ app/src/main/java/com/example/tomasz/studiesmenager/Menager.java
@@ -0,0 +1,13 @@
+package com.example.tomasz.studiesmenager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menager);
+    }
+}

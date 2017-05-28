481ffc7ee0cd4603006cf8f0db50e33ffe4ae488
pmoren <p.moren15@gmail.com>
2017-05-28 08:20:33 +0200

    dzialajace pushe

--- app/src/main/java/com/example/tomasz/studiesmenager/Main.java
+++ app/src/main/java/com/example/tomasz/studiesmenager/Main.java
@@ -1,5 +1,8 @@
 package com.example.tomasz.studiesmenager;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -8,6 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.tomasz.studiesmenager.Model.Attendence;
@@ -18,6 +22,7 @@ import com.example.tomasz.studiesmenager.addView.addViewActivity;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 
 public class Main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -187,6 +192,73 @@ public class Main extends AppCompatActivity
             a.WasPresent = false;
             a.save();
         }
+
+        ////////////////////////////////////////////////////////////////////////////
+
+
+        Subject s = new Subject();
+        s.Name = "Algebra";
+        s.save();
+
+        Class c = new Class();
+        c.Type = Class.ClassType.Lab;
+        c.EndHour = new Date(1996, 12, 10);
+        c.StartHour = new Date(1996, 12, 10);
+        c.FreqInWeeks = 1;
+        c.MaxScore = 100;
+        c.MinPassScore = 50;
+        c.Subject = s;
+        c.save();
+
+        Attendence a = new Attendence();
+        a.WasPresent = true;
+        a.PointsEarned = 0;
+        a.Class = c;
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(Calendar.YEAR, 1988);
+        cal2.set(Calendar.MONTH, Calendar.JANUARY);
+        cal2.set(Calendar.DAY_OF_MONTH, 1);
+        a.Date = cal2.getTime();
+        a.save();
+        Log.w("myapp--", a.getId().toString());
+        Attendence a2 = new Attendence();
+        a2.WasPresent = true;
+        a2.PointsEarned = 0;
+        a2.Class = c;
+        Calendar cal3 = Calendar.getInstance();
+        cal3.set(Calendar.YEAR, 1999);
+        cal3.set(Calendar.MONTH, Calendar.JANUARY);
+        cal3.set(Calendar.DAY_OF_MONTH, 1);
+        a2.Date = cal3.getTime();
+        a2.save();
+        Log.w("myapp--", a2.getId().toString());
+        //Push intent i alarmmanager
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent notificationIntent = new Intent("android.media.action.DISPLAY_NOTIFICATION");
+        notificationIntent.putExtra("Present",a.WasPresent);
+        notificationIntent.putExtra("Date",a.Date.toString());
+        notificationIntent.putExtra("ID",a.getId());
+        notificationIntent.addCategory("android.intent.category.DEFAULT");
+
+        Intent notificationIntent2 = new Intent("android.media.action.DISPLAY_NOTIFICATION");
+        notificationIntent2.putExtra("Present",a2.WasPresent);
+        notificationIntent2.putExtra("Date",a2.Date.toString());
+        notificationIntent2.putExtra("ID",a2.getId());
+        notificationIntent2.addCategory("android.intent.category.DEFAULT");
+
+
+
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, new Random().nextInt(3929), notificationIntent, PendingIntent.FLAG_ONE_SHOT);
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 1);
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast);
+        cal.add(Calendar.SECOND, 4);
+        PendingIntent broadcast2 = PendingIntent.getBroadcast(this, new Random().nextInt(3929), notificationIntent2, PendingIntent.FLAG_ONE_SHOT);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast2);
+
+
 
     }
 
--- app/src/main/java/com/example/tomasz/studiesmenager/ScheduleAdapterCardView.java
+++ app/src/main/java/com/example/tomasz/studiesmenager/ScheduleAdapterCardView.java
@@ -8,9 +8,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 
-//import com.example.tomasz.studiesmenager.Model.Attendence;
 import com.example.tomasz.studiesmenager.Model.Attendence;
-import com.example.tomasz.studiesmenager.R;
+
 
 import java.util.Calendar;
 import java.util.List;
